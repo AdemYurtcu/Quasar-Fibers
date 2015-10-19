b4263e09a5f63f2ab81be63062f8f7f3c01e8aca
Adem Yurtcu <ademyurtcu@gmail.com>
2015-10-19 17:55:05 +0300

    implement commit

--- src/main/java/com/foreks/feed/QFTask1.java
+++ src/main/java/com/foreks/feed/QFTask1.java
@@ -1,6 +1,10 @@
 package com.foreks.feed;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.ExecutionException;
@@ -12,43 +16,63 @@ import co.paralleluniverse.strands.channels.Channel;
 import co.paralleluniverse.strands.channels.Channels;
 
 public class QFTask1 {
-    private static final Channel<String> channel = Channels.newChannel(1000, Channels.OverflowPolicy.BLOCK, true, true);
+
+    private static final Channel<String> channel = Channels.newChannel(5, Channels.OverflowPolicy.BLOCK, true, true);
 
     public static void main(final String[] args) throws IOException, ExecutionException, InterruptedException, SuspendExecution {
+        writer();
         reader();
-        System.out.println("Task is complated.");
+        System.out.println("Task is complated");
     }
-    private static int i = 0;
+    static int i;
 
-    private static void reader() throws IOException {
-
-        Files.lines(Paths.get("File.txt")).forEach(s -> {
-            i++;
-
-            final Fiber<Void> writer = new Fiber<Void>("reader " + i, new SuspendableRunnable() {
-
+    private static void writer() throws ExecutionException, InterruptedException {
+        for (i = 0; i < 500; i++) {
+            final Fiber<Void> writer = new Fiber<Void>("writer " + i, new SuspendableRunnable() {
                 private static final long serialVersionUID = 1L;
 
                 @Override
                 public void run() throws SuspendExecution, InterruptedException {
-                    final String receive = channel.receive();
-                    System.out.println(receive);
+                    final Channel<String> templateChannel = channel;
+                    final File f = new File("Results/writer" + i + ".txt");
+                    if (!f.exists()) {
+                        try {
+                            f.createNewFile();
+                        } catch (final IOException e) {
+                            System.out.println(e.getMessage());
+                        }
+
+                    } else {
+                        PrintWriter pWriter = null;
+                        try {
+                            pWriter = new PrintWriter(new BufferedWriter(new FileWriter(f, true)));
+                            while (templateChannel.isClosed() == true) {
+                                final String receive = templateChannel.receive();
+                                pWriter.println(receive);
+                            }
+
+                        } catch (final IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                        pWriter.close();
+                    }
+
                 }
             }).start();
-            final String message = s + " " + writer.getName();
-            try {
-                channel.send(message);
-            } catch (final Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            try {
-                writer.join();
-            } catch (final Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            writer.join();
+        }
+    }
 
+    private static void reader() throws IOException {
+
+        Files.lines(Paths.get("File.txt")).forEach(s -> {
+            try {
+                channel.send(s);
+            } catch (final Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         });
     }
 
