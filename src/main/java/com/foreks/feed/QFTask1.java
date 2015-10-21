7e25c0788fa68fc71025c20a6c3bfb18f54c8216
Adem Yurtcu <ademyurtcu@gmail.com>
2015-10-21 10:14:53 +0300

    implement commit

--- src/main/java/com/foreks/feed/QFTask1.java
+++ src/main/java/com/foreks/feed/QFTask1.java
@@ -1,17 +1,17 @@
 package com.foreks.feed;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import static com.foreks.feed.UtilException.rethrowConsumer;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.fibers.SuspendExecution;
-import co.paralleluniverse.strands.SuspendableRunnable;
 import co.paralleluniverse.strands.channels.Channel;
 import co.paralleluniverse.strands.channels.Channels;
 
@@ -26,60 +26,30 @@ public class QFTask1 {
     static int i;
 
     private static void writer() throws ExecutionException, InterruptedException {
-        for (i = 0; i < 1000; i++) {
-            new Fiber<Void>("writer " + i, new SuspendableRunnable() {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public void run() throws SuspendExecution, InterruptedException {
-                    Channel<String> channel;
-                    try {
-                        channel = createChannel();
-                        final File f = new File("Results/writer" + i + ".txt");
-                        if (!f.exists()) {
-                            try {
-                                f.createNewFile();
-                            } catch (final IOException e) {
-                                System.out.println(e.getMessage());
-                            }
-                        }
-                        PrintWriter pWriter = null;
-
-                        try {
-                            pWriter = new PrintWriter(new BufferedWriter(new FileWriter(f, true)));
-                        } catch (final IOException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                        while (!channel.isClosed()) {
-                            pWriter.println(channel.receive());
-                        }
-
-                        pWriter.close();
-                    } catch (final IOException e1) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace();
-                    }
-
+        final Channel<String> channel = Channels.newChannel(10000, Channels.OverflowPolicy.BLOCK, true, false);
+        final List<Fiber<Void>> fibers = IntStream.range(0, 10)
+                                                  .mapToObj((final int index) -> new Fiber<Void>("writer "
+                                                                                                 + index,
+                                                                                                 new com.foreks.feed.FileWriterFiber(index, channel)))
+                                                  .peek(rethrowConsumer(f -> f.start()))
+                                                  .collect(Collectors.toList());
+        try {
+            Files.lines(Paths.get("File.txt")).forEach(s -> {
+                try {
+                    System.out.println("msg ->" + s);
+                    channel.send(s);
+                } catch (final Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-
-            }).start().join();
+            });
+        } catch (final IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-
-    }
-
-    public static Channel<String> createChannel() throws IOException {
-        final Channel<String> channel = Channels.newChannel(1200, Channels.OverflowPolicy.BLOCK, true, true);
-        Files.lines(Paths.get("File.txt")).forEach(s -> {
-            try {
-                channel.send(s);
-            } catch (final Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        });
         channel.close();
-        return channel;
+        fibers.stream().forEach(rethrowConsumer(f -> f.join()));
+
     }
 
 }
